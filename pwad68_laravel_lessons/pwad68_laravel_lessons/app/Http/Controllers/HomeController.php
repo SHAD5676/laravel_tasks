0c356c0e12e2b4f2c630706f25441efbb574bc54
Shad <shadjannat1@gmail.com>
2026-01-06 13:29:07 +0600

    first commit

---
+++ pwad68_laravel_lessons/pwad68_laravel_lessons/app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       
+       $students = Student::all();
+       //return print_r($students);
+       
+        return view('home', ['students'=>$students, 'title'=>"All students"]);
+    }
+
+     public function about()
+    {
+        return view('about');
+    }
+
+     public function contact()
+    {
+        return view('contact');
+    }
+
+
+
+   
+}
